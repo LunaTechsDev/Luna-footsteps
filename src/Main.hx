b17a8da81fafc2bed0d595ae7471580fb4be59df
Kino  Rose <kinoarose@gmail.com>
2020-09-27 18:22:58 -0400

    Initial commit

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,5 @@
 import rm.core.JsonEx;
 import macros.FnMacros;
-import rm.scenes.Scene_Title;
 import js.Syntax;
 import pixi.interaction.EventEmitter;
 import core.Amaryllis;
@@ -14,36 +13,27 @@ using core.NumberExtensions;
 using StringTools;
 using utils.Fn;
 
-typedef LinkWindowInfo = {
-  var x: Int;
-  var y: Int;
-  var width: Int;
-  var height: Int;
-  var backgroundType: Int;
-  var link: String;
-  var image: String;
-}
+typedef LinkWindowInfo = {}
+typedef LParams = {}
 
-typedef LParams = {
-  var linkWindows: Array<LinkWindowInfo>;
-}
-
-@:native('LunaLinks')
-@:expose('LunaLinks')
+@:native('LunaFootsteps')
+@:expose('LunaFootsteps')
 class Main {
   public static var Params: LParams = null;
   public static var listener: EventEmitter = Amaryllis.createEventEmitter();
 
   public static function main() {
-    var plugin = Globals.Plugins.filter((plugin) -> ~/<LunaLinks>/ig.match(plugin.description))[0];
+    var plugin = Globals.Plugins.filter((plugin) -> ~/<LunaFootsteps>/ig.match(plugin.description))[0];
     var params = plugin.parameters;
     untyped Params = {
       linkWindows: JsonEx.parse(params['linkWindows']).map((win) -> JsonEx.parse(win))
     }
     trace(Params);
 
-    Comment.title('Scene_Title');
-    FnMacros.jsPatch(true, Scene_Title, TitlePatch);
+    Comment.title('Scene_Map');
+    FnMacros.jsPatch(true, Scene_Title, MapPatch);
+
+    Comment.title('Game_Player');
   }
 
   public static function params() {
